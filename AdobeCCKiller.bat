172fdccf1b3640f3f3db165473ef7423824f3098
viniciusfos1996 <47922771+viniciusfos1996@users.noreply.github.com>
2019-11-06 01:27:18 -0300

    0.1b
    change xxx to the name of the adobe app when closed the script will kill Creative Cloud

---
+++ AdobeCCKiller.bat
@@ -0,0 +1,8 @@
+@echo off
+set EXE=adobeprocessname.exe
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% goto FOUND
+taskkill /F /IM "Adobe Desktop Process.exe" & taskkill /F /IM "Adobe Desktop Service.exe" & taskkill /F /IM AdobeIPCBroker.exe & taskkill /F /IM AdobeUpdateService.exe & taskkill /F /IM CCXProcess.exe & taskkill /F /IM CCLibrary.exe & taskkill /F /IM AdobeNotificationClient.exe & taskkill /F /IM armsvc.exe & taskkill /F /IM AGSService.exe & taskkill /F /IM CoreSync.exe & taskkill /F /IM "Adobe Installer.exe" & taskkill /F /IM CreativeSDKAppServiceClient.exe & taskkill /F /IM backgroundTaskHost.exe & taskkill /F /IM RuntimeBroker.exe & taskkill /F /IM Node.exe
+goto FIN
+:FOUND
+:FIN
+exit
